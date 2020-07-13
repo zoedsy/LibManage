a045a8602252c1964d9eed36ddcd3b42e465c844
张三 <7381669+HughNew@user.noreply.gitee.com>
2020-07-13 21:28:11 +0800

    update for class

--- include/library.h
+++ include/library.h
@@ -5,31 +5,43 @@
 #include<map>
 
 namespace LibSys{
-    using Str=std::string;
     enum Category{MAGAZINE,TEXTBOOK,PRIMER,AUTOBIOGRAPHY,FAIRY,NOVEL,ADVENTURE,FICTION,SUNDRIES};
+    struct ISBN{
+        std::string isbn;
+        bool operator==(ISBN const&i)const noexcept{return isbn==i.isbn;}
+    };
     /**
      * @brief the book class which contains the all info of book itself
     */
     class Book{
         private:
-            Str name;
+            std::string name,author,press;
             int*count;
             const Category cate;
         public:
-            const Str ISBN;
+            const ISBN isbn;
             Book()=delete;
-            Book(Str n,Str isbn,int const&c,Category cg);
+            Book(std::string n,std::string isbn,std::string pr,int const&c,Category cg);
             Book(Book const&)noexcept;
-            ~Book();
-            Book& operator=(Book const&);
-            const Str GetName()const noexcept{return name;}
+            /**
+             * @brief remember to delete the pointer [count]
+            */
+            ~Book()noexcept;
+            Book& operator=(Book const&)noexcept;
+            bool operator==(Book const&b){return isbn==b.isbn;}
+            inline const std::string GetName()const noexcept{return name;}
+            inline const std::string GetPress()const noexcept{return press;}
+            inline const std::string GetAuthor()const noexcept{return author;}
+            inline const std::string GetIsbn()const noexcept{return isbn.isbn;}
+            inline bool borrow()noexcept{return --*count==0;}
+            inline void ret()noexcept{++*count;}
     };
     struct visitor_tag{};
     struct member_tag:visitor_tag{};
     struct admin_tag:member_tag{};
     class visitor{
         protected:
-            Str name;
+            std::string name;
             /*
             data
             */
@@ -38,27 +50,83 @@ namespace LibSys{
             typedef visitor_tag tag;
 
     };
-    enum access{MEMBER,ADMIN};
     class member:public visitor{
         private:
         //data
         public:
-            constexpr static access ACCESS=MEMBER;
             typedef member_tag tag;
     };
     class admin:public member{
         private:
         public:
-            constexpr static access ACCESS=ADMIN;
             typedef admin_tag tag;
     };
+    class Message final{
+        std::string time,person,action;
+        public:
+        Message(std::string const&t,std::string const&p,std::string const&a)noexcept
+            :time(t),person(p),action(a){}
+        std::string operator()()noexcept{return time+" : "+person+" "+action;}
+    };
+    /**
+     * @brief remember to log every action
+    */
     class library{
         private:
             std::map<std::string,Book> BooksMap;
             std::map<visitor*,Book>borrow_trace;
+            std::string DestFile;
+            /**
+             * @brief fetch the data from file
+            */
+            void update(std::string const&file=DefaultFile)noexcept;
+            /**
+             * @brief set log file which can't be changed by anyone
+            */
+            static constexpr char*LOGFILE="";
+            void log(Message const&m)noexcept;
         public:
-
+            constexpr static char*DefaultFile="";//default file set
+            /**
+             * @brief default constructor 
+             * using the default file to fetch data
+            */
+            library()noexcept{update();}
+            ~library()noexcept;
+            /**
+             * @brief assign the file to reload
+             * @param file the assigned file
+            */
+            library(std::string const&file)noexcept{update(file);}
+            /**
+             * @brief set the data file before exit
+             * if not set, use the default file to keep the data
+             * @return return current file set for storage
+            */
+            std::string setDestFile(std::string const&)noexcept;
+            /**
+             * @brief save data to specific file
+            */
+            void save(std::string const&file="");
+            /**
+             * @brief search book by bookname or else
+            */
+            bool borrow(member const&,std::string const&)noexcept;
+            /**
+             * @brief search book by ISBN
+            */
+            bool borrow(member const&,ISBN const&)noexcept;
+            /**
+             * @brief search book with complete info
+            */
+            bool borrow(member const&,Book const&)noexcept;
+            /**
+             * @brief return book
+            */
+            bool ret(member const&,Book const&)noexcept;
+            bool changeBookName(admin const&,Book const&,std::string const&);
+            bool changeBookName(admin const&,ISBN const&,std::string const&);
+            void buy(admin const&,Book const&)noexcept;
     };
-
 }
 #endif//_LIBRARY_H_
--- src/book.cpp
+++ src/book.cpp
@@ -1,7 +1,7 @@
 #include"library.h"
 #include<iostream>
 namespace LibSys{
-    Book::Book(Str n,Str isbn,int const&c,Category cg)try
-        :name(n),ISBN(isbn),count(new int(c)),cate(cg){}
+    Book::Book(std::string n,std::string is,std::string pr,int const&c,Category cg)try
+        :name(n),isbn({is}),press(pr),count(new int(c)),cate(cg){}
         catch(std::bad_alloc const&){std::cerr<<"out of the contain of library"<<std::endl;}
 }
