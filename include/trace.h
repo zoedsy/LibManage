763f9fac3a79ef790213ee3e4b801fb223a8c795
SunnyHaze <mxch1122@126.com>
2020-07-15 14:24:31 +0800

    完成了trace类的实现，暂未经过测试。

--- include/trace.h
+++ include/trace.h
@@ -16,8 +16,8 @@ private:
     std::list<trace::tip>::iterator it = book_trace.begin();
 
 public:
-    trace();
-    void append(std::string NAME, std::string ISBN, std::string lend)
+    trace() = default;
+    void append(std::string NAME, std::string ISBN, std::string LEND_TIME)
     {
         tip temp;
         temp.name = NAME;
@@ -29,30 +29,116 @@ public:
     void search_people(std::string);
     //展示全部信息
     void show_all();
-    //通过姓名删除（代表还书）
-    void remove_people(std::string);
+    //通过姓名删除（代表还书）   特别的，返回值为借书时间的字符串
+    std::string remove_people(std::string);
     //通过show_all的序号删除（代表还书）
-    void remove_ID(int n);
+    std::string remove_ID(int n);
 
-    ~trace();
+    ~trace() = default;
 };
 void trace::show_all()
 {
     std::cout << "序号\tISBN\t借书人\t借书时间\n";
     int size = book_trace.size();
-    for (int i = 1; i <= size; i++)
+    if (size != 0)
     {
-        std::cout << i << "\t" << it->isbn << it->name << it->lend_time << std::endl;
-        it++;
+        for (int i = 1; i <= size; i++)
+        {
+            std::cout << i << "\t" << it->isbn << it->name << it->lend_time << std::endl;
+            it++;
+        }
+        std::cout << std::endl;
+        it = book_trace.begin();
     }
-    std::cout << std::endl;
-    it = book_trace.begin();
+    else
+    {
+        std::cout << "没有借阅记录";
+    }
 }
-
-trace::trace(/* args */)
+void trace::search_people(std::string name)
 {
+    std::cout << "正在按人查找信息" << std::endl;
+    int size = book_trace.size();
+    if (size != 0)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            if (it->name == name)
+                std::cout << i << "\t" << it->isbn << it->name << it->lend_time << std::endl;
+            it++;
+        }
+        std::cout << std::endl;
+        it = book_trace.begin();
+    }
+    else
+    {
+        std::cout << "没有借阅记录";
+    }
 }
-
-trace::~trace()
+std::string trace::remove_people(std::string name)
 {
+    std::cout << "正在按人删除信息" << std::endl;
+    int size = book_trace.size();
+    if (size == 0)
+    {
+        std::cout << "没有借阅记录!无法删除！";
+        return "";
+    }
+    else
+    {
+        bool flag = 1;
+        for (int i = 1; i <= size; i++)
+        {
+            if (it->name == name)
+            {
+                size--;
+                std::cout << "正在移除借阅记录" << std::endl;
+                std::cout << i << "\t" << it->isbn << it->name << it->lend_time << std::endl;
+                std::string temp = it->lend_time;
+                book_trace.erase(it);
+                flag = 0;
+                return temp;
+            }
+            it++;
+        }
+        if (flag)
+            std::cout << "没有找到该姓名！";
+        it = book_trace.begin();
+        return "";
+    }
+}
+std::string trace::remove_ID(int n)
+{
+    std::cout << "正在按序号删除信息" << std::endl;
+    int size = book_trace.size();
+    if (size == 0)
+    {
+        std::cout << "没有借阅记录！无法删除";
+        return "";
+    }
+    else if (n > size || n < 0)
+    {
+        std::cout << "请输入正确的序号！";
+        return "";
+    }
+    else if
+    {
+        for (int i = 1; i <= size; i++)
+        {
+
+            if (i == n)
+            {
+                size--;
+                std::cout << "正在移除借阅记录" << std::endl;
+                std::cout << i << "\t" << it->isbn << it->name << it->lend_time << std::endl;
+                std::string temp = it->lend_time;
+                book_trace.erase(it);
+                return temp;
+            }
+            it++;
+        }
+        it = book_trace.begin();
+        return "";
+    }
+}
 }
